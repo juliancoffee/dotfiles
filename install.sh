3e986bc4053e3a828de5c2ccbc0bb3f671afcaff
juliancoffee <lightdarkdaughter@gmail.com>
2020-07-10 01:15:16 +0300

    set up i3

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
+#!/usr/bin/env sh
+
 if [ -e "$HOME/.config/nvim" ] 
 then
     echo "nvim config exists: pass"
     echo "do smth about that"
 else
     ln -s "$HOME/dotfiles/editors/nvim" "$HOME/.config/nvim"
+    echo "nvim config set up"
+fi
+
+if [ -e "$HOME/.config/i3" ] 
+then
+    echo "i3 config exists: pass"
+    echo "do smth about that"
+else
+    ln -s "$HOME/dotfiles/wm/i3" "$HOME/.config/i3"
+    echo "i3 config set up"
 fi
