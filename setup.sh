8f80e57803db4f2e2b030c0d0800301f4b25e8d6
juliancoffee <lightdarkdaughter@gmail.com>
2020-07-10 16:02:08 +0300

    update .mostrc to more gentle colors update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,70 @@
 #!/usr/bin/env sh
 
-if [ -e "$HOME/.config/nvim" ] 
-then
-    echo "nvim config exists: pass"
-    echo "do smth about that"
-else
-    ln -s "$HOME/dotfiles/editors/nvim" "$HOME/.config/nvim"
-    echo "nvim config set up"
-fi
+error_color() {
+    printf "\x1b[31m" #red for error
+}
 
-if [ -e "$HOME/.config/i3" ] 
-then
-    echo "i3 config exists: pass"
-    echo "do smth about that"
-else
-    ln -s "$HOME/dotfiles/wm/i3" "$HOME/.config/i3"
-    echo "i3 config set up"
-fi
+warn_color() {
+    printf "\x1b[33m"
+}
+
+ok_color() {
+    printf "\x1b[32m"
+}
+
+reset_color() {
+    printf "\x1b[0m"
+}
+
+setup() {
+    local src="$1"
+    local dest="$2"
+    local name="$3"
+
+    # check if link exists
+    if [ -L "$dest" ]
+    then
+        if [ -e "$dest" ]
+        then
+            warn_color
+            echo "$name : symbolic link already set"
+            reset_color
+
+            return
+        else
+            error_color
+            echo "$name : symbolic link set, but broken"
+            reset_color
+
+            return
+        fi
+    fi
+
+    # check if destination already exists
+    if [ -e "$dest" ] 
+    then
+        error_color
+        echo "$name : config exists: skip"
+        reset_color
+
+        return
+    fi
+
+    # do set up
+    if ln -s "$src" "$dest"
+    then
+        ok_color
+        echo "$name : config set up from $src to $dest"
+        reset_color
+    else
+        error_color
+        echo "$name : something went wrong with setting config"
+        reset_color
+    fi
+}
+
+DOTS=$(dirname "$0")
+echo "===== Dotfiles root in $DOTS ====="
+setup "$DOTS/editors/nvim" "$HOME/.config/nvim" "Neovim"
+setup "$DOTS/wm/i3" "$HOME/.config/i3" "i3"
+setup "$DOTS/pagers/most/.mostrc" "$HOME/.mostrc" "most"
