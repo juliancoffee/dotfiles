938dfd64f50d57ec3eea525843c659486c5e770e
juliancoffee <lightdarkdaughter@gmail.com>
2022-11-09 16:45:18 +0200

    (fish) remove redundant stuff

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -9,10 +9,6 @@ function fish_greeting
     cal
 end
 
-function fish_prompt
-    larta
-end
-
 function fish_command_not_found
     __fish_default_command_not_found_handler $argv[1]
 end
@@ -24,48 +20,36 @@ export MANPAGER=most
 
 #plugins and other
 export PYENV_ROOT="$HOME/.local/share/pyenv"
-export ORACLE_HOME="/usr/lib/oracle/product/11.2.0/xe"
-export ORACLE_SID="XE"
 
 set PATH $PATH $WORK/bin
 set PATH $PATH $HOME/.poetry/bin
-set PATH $PATH $ORACLE_HOME/bin
-set PATH $PATH $HOME/repos/v/bin
 set PATH $PATH $HOME/.cargo/bin
 set PATH $PATH $HOME/.opam/default/bin
-set PATH $PATH $HOME/repos/ion/target/release
 set PATH $PATH $HOME/.local/bin
 set PATH $PATH $HOME/.yarn/bin
 set PATH $PATH $HOME/.gem/ruby/2.7.0/bin
 set PATH $PATH $PYENV_ROOT/bin
 set PATH $PATH $HOME/.pub-cache/bin
 set PATH $PATH $HOME/.nix-profile/bin
-set PATH $PATH $HOME/.emacs.d/bin
 
-#some variables
+#some local variables
 set -l TMUX_rc "$HOME/.tmux.conf"
 set -l SHELL_rc "$HOME/.config/fish/config.fish"
 set -l WM_rc "$HOME/.config/i3/config"
-set -l THEME_rc "$HOME/.config/awesome/theme.lua"
 
 set -l NVIM_rc "$HOME/.config/nvim/init.vim"
-set -l EMACS_rc "$HOME/.emacs.d/init.el"
 
 #Alias for edit configs
 # Shell
 abbr --add edsh "$EDITOR $SHELL_rc" # fish
 # Editors
 abbr --add edvim "$EDITOR $NVIM_rc" # nvim
-alias edem="$EDITOR $EMACS_rc" # emacs
 # Window managers
-alias edwm="$EDITOR $WM_rc" # awesome wm
-alias edtheme="$EDITOR $THEME_rc" # theme
+alias edwm="$EDITOR $WM_rc" # wm
 # Terminals
 alias edterm="$EDITOR $HOME/.config/alacritty/alacritty.yml"
 # Terminal multiplexor
 alias edtm="$EDITOR $TMUX_rc"
-alias edhello="cd $HOME/Workspace/scripts/tmux"
-
 
 #some ls aliases
 alias la="ls -a"
@@ -109,16 +93,6 @@ function cdwork
 end
 complete -c cdwork -x -a "(__fish_complete_directories ($WORK))" 
 
-function cdtest
-    cdwork "test/$argv[1]"
-end
-complete -c cdtest -x -a "(__fish_complete_directories ($WORK/test))" 
-
-function cdcode
-    cdwork "codewars/$argv[1]"
-end
-complete -c cdcode -x -a "(__fish_complete_directories ($WORK/codewars))" 
-
 function cdconf 
     cd "$CONFIG/$argv[1]"
 end
@@ -128,26 +102,6 @@ function cdload
     cd "$DOWNLOADS/$argv[1]"
 end
 complete -c cdload -x -a "(__fish_complete_directories ($DOWNLOADS))" 
-
-
-#git
-abbr --add g "git"
-abbr --add gdiff "git diff"
-abbr --add gstat "git status"
-abbr --add gaddall "git add -A"
-abbr --add gcomm "git commit"
-abbr --add gclon "git clone"
-
-#tmux
-function start 
-    $HOME/Workspace/scripts/tmux/scripts/start.py
-end
-function port 
-    $HOME/Workspace/scripts/tmux/scripts/second.py
-end
-function hello 
-    $HOME/Workspace/scripts/tmux/scripts/hello.py
-end
 
 #dotfiles
 set -U DOTFILES "$HOME/.config/dotfiles"
