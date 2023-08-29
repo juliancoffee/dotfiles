b8a620da6b921db5ca09d37279fc4a303a521d96
juliancoffee <lightdarkdaughter@gmail.com>
2023-08-29 11:36:10 +0300

    (scripts) Update efi.sh script
    - Made it no-op, because risk of accidentally running it is too high and
      it has way to many parameters

--- scripts/efi.sh
+++ scripts/efi.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env sh
 
-if [ -n "$PARTITION" ]
-then
-efibootmgr \
+echo "This script is no-op, edit source code to run it."
+echo
+echo "For your information, it has a very high chance to break your system,"
+echo "so make sure you know what you're doing."
+echo
+echo "Run efibootmgr --help"
+
+# In this example we create efi entry with following:
+#
+# our boot partition is /dev/sda5, which is specified by --disk and --part
+# our label is Arch Linux (can be anything)
+# our loader is /boot/vmlinuz-linux
+# our root is on /dev/sda6
+# our initramfs is /boot/initramfs-linux.img
+true efibootmgr \
     --create \
     --disk /dev/sda \
-    --part "${PARTITION:?please specify \$PARTITION variable}" \
-    --label "Arch Linux (LTS)" \
+    --part 5 \
+    --label "Arch Linux" \
     --loader /vmlinuz-linux \
     --unicode 'root=/dev/sda6 rw initrd=\initramfs-linux.img'
-else
-    echo "PARTITION variable is unset."
-fi
