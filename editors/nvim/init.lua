13457e992e21d000c67817f86258750f4ad60f45
juliancoffee <lightdarkdaughter@gmail.com>
2023-08-31 12:26:34 +0300

    (nvim) add more stuff
    - Add comments
    - Add expandtab settings
    - Add listchars settings

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -1,6 +1,24 @@
-vim.o.termguicolros = true
+-- show pretty colors
+vim.o.termguicolors = true
+
+-- shows numbers
 vim.o.number = true
 vim.o.relativenumber = true
+
+-- replace <tab> with spaces
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+-- show sneaky characters
+vim.o.list = true
+vim.opt.listchars = {
+    trail = ".",
+    tab = "> ",
+}
+
+-- set bigger limit to allowed number of pages opened with "-p"
 vim.o.tabpagemax = 500
 
-vim.cmd.colorscheme('habamax')
+-- default yet pretty colorscheme
+vim.cmd.colorscheme('desert')
