1b166c1997d89beabf439eead16fea6f55403756
juliancoffee <lightdarkdaughter@gmail.com>
2023-09-02 17:20:24 +0300

    (nvim) package manager and colorscheme
    - Use lazy.nvim as package manager, there is a big chance I would use
      packer.nvim, but it got unmaintained like week before I started
      updating my neovim config.
    - Use catppuccin as my colorscheme.
        I quickly discarded muclor, which is a fork of onehalfdark, because I
        have no idea how to license forks.
        I then tried onehalfdark, but it doesn't work that nice with
        lazy.nvim and it's old-style plugin written in vimscript.
        I then tried gruvbox, but for some reason, I just don't like it.
        I then tried tokyonight, but it's just too blue.
        So, yeah. Catppuccin seems nice.

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -1,4 +1,78 @@
 --
+-- package management
+--
+
+-- bootstrap lazy.nvim package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- convinient way to swap between colorschemes
+local colorscheme0 = {
+    plug = {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    run = function()
+        require("tokyonight").setup({
+            style = "moon",
+            styles = {},
+        })
+        vim.cmd [[ colorscheme tokyonight ]]
+    end
+}
+
+local colorscheme = {
+    plug = {
+        "catppuccin/nvim", name = "catppuccin", priority = 1000,
+    },
+    run = function()
+        require("catppuccin").setup({
+            flavour = "frappe"
+        })
+        vim.cmd [[ colorscheme catppuccin ]]
+    end
+}
+
+local lazy_config = {
+    ui = {
+        icons = {
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+            lazy = "💤 ",
+        },
+    },
+}
+
+require("lazy").setup({
+    {
+        colorscheme.plug,
+    },
+}, lazy_config)
+
+colorscheme.run()
+
+--
 -- options
 --
 
@@ -28,10 +102,6 @@ vim.opt.listchars = {
 
 -- set bigger limit to allowed number of pages opened with "-p"
 vim.o.tabpagemax = 500
-
--- default yet pretty colorscheme
-vim.cmd.colorscheme('desert')
-
 
 --
 -- keybinds
