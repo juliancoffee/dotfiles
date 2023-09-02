53afc13d8e1dad4686e42a95a27b8a7d01424f02
juliancoffee <lightdarkdaughter@gmail.com>
2023-09-02 15:36:32 +0300

    (nvim) add keymaps

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -1,3 +1,7 @@
+--
+-- options
+--
+
 -- show pretty colors
 vim.o.termguicolors = true
 
@@ -9,6 +13,11 @@ vim.o.relativenumber = true
 vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
+
+-- vim has these options, but it seems to work just fine without them
+-- I'll leave them just in case, but i'm not sure if they're still useful
+-- vim.o.autoindent = true
+-- vim.o.smartindent = true
 
 -- show sneaky characters
 vim.o.list = true
@@ -22,3 +31,16 @@ vim.o.tabpagemax = 500
 
 -- default yet pretty colorscheme
 vim.cmd.colorscheme('desert')
+
+
+--
+-- keybinds
+--
+
+-- disable accidental q key-press
+vim.keymap.set({'n', 'v'}, 'q:', '<Nop>') -- supposed to open cmdline window
+vim.keymap.set('n', 'Q', '<Nop>') -- idk
+
+-- disable search highlight
+-- <C-_> actually means <C-/>, don't ask me why
+vim.keymap.set('n', '<C-_>', ':nohlsearch<CR>')
