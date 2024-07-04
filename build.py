484e1e1c4d8a7b716cde8f2d3de26052233c8004
juliancoffee <lightdarkdaughter@gmail.com>
2024-07-04 16:22:20 +0300

    Add options in the new setup and improved logic
    - Adds variable config entries to specify multiple configs to pick from
      by the user.
    - Checks existence of files/links/directories before running linkage and reports
    findings.
    - Adds nvim, mpv and alacritty configs to setup script.

--- build.py
+++ build.py
@@ -52,19 +52,60 @@ def print_warn(msg: str) -> None:
 def print_err(msg: str) -> None:
     print_colored(msg, ColorCode.RED)
 
+def make_choice(desc: str, choices: list[tuple[str, Path]]) -> tuple[str, Path]:
+    print(f"There are multiple choices for {desc}:")
+    for (i, (desc, path)) in enumerate(choices):
+        print(f"{i}) {desc} <> {path}")
+
+    while True:
+        try:
+            request = input("Please type a number to pick: ")
+            idx = int(request)
+            return choices[idx]
+        except ValueError:
+            print_err("failed to parse the number, please try again")
+
+def handle_existence(desc: str, dest: Path) -> bool:
+    if dest.exists():
+        print(f"{desc}:")
+        if dest.is_symlink():
+            print_warn(f"it's already there, it's a [symlink to {dest.resolve()}]")
+        else:
+            print_warn(f"it's already there")
+        if dest.is_file():
+            print_warn("it's a [plain file]")
+        if dest.is_dir():
+            file_num = 0
+            dir_num = 0
+            link_num = 0
+            for child in dest.iterdir():
+                if child.is_file():
+                    file_num += 1
+                if child.is_dir():
+                    dir_num += 1
+                if child.is_symlink():
+                    link_num += 1
+
+            print_warn(
+                    f"it's a [directory with "
+                    f"{file_num} files, "
+                    f"{dir_num} dirs, "
+                    f"{link_num} links]"
+            )
+        print_warn("skipping")
+        return True
+    return False
+
 def setup_link(desc: str, src: Path, dest: Path) -> None:
-    try:
-        dest.symlink_to(src)
-        print_ok(f"{desc}: {dest} => {src}")
-    except FileExistsError as e:
-        print_warn(
-                f"{desc}: failed to create the symlink, it probably already exists"
-        )
-        print_warn(str(e))
+    if handle_existence(desc, dest):
+        return
+
+    dest.symlink_to(src)
+    print_ok(f"{desc}: {dest} => {src}")
 
 def main():
     home = Path.home()
-    print_warn("Assuming current directory as the dotfiles root")
+    print("Assuming current directory as the dotfiles root")
     dotfiles = Path.cwd()
 
     simple_configs = [
@@ -72,13 +113,37 @@ def main():
         ("tmux", dotfiles / "tmux/.tmux.conf", home / ".tmux.conf"),
         # zsh too
         ("zsh", dotfiles / "shells/zsh/zshrc", home / ".zshrc"),
+        # mpv is a good boi
+        ("mpv", dotfiles / "other/mpv", home / ".config/mpv"),
+        ("nvim", dotfiles / "editors/nvim", home / ".config/nvim"),
     ]
 
     option_configs = [
-        ("alacritty-zsh")
+        # alacritty has options mainly because of shell usage
+        #
+        # On my Linux machines (with i3wm, at least) I just make a keybind to
+        # run alacritty with tmux, plus I use fish.
+        #
+        # On MacOS, I can't (or don't know how) edit what button on by bar does
+        # so instead, I specify how the shell should be invoked in the config,
+        # which also runs tmux, but also zsh.
+        (
+            "alacritty",
+            [
+                ("mac", dotfiles / "terminals/alacritty-mac"),
+            ],
+            home / ".config/alacritty",
+
+        ),
     ]
 
     for (desc, src, dest) in simple_configs:
         setup_link(desc, src, dest)
+
+    for (desc, choices, dest) in option_configs:
+        if handle_existence(desc, dest):
+            continue
+        choice, src = make_choice(desc, choices)
+        setup_link(f"desc/{choice}", src, dest)
 
 main()
