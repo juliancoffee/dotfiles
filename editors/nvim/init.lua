c550802f92ffc08cbcebfa869816ef5f70f1bca8
juliancoffee <lightdarkdaughter@gmail.com>
2025-04-15 15:58:10 +0300

    Add telescope.nvim

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -23,8 +23,9 @@ vim.opt.shiftwidth = 4
 
 -- vim has these options, but it seems to work just fine without them
 -- I'll leave them just in case, but i'm not sure if they're still useful
--- vim.o.autoindent = true
--- vim.o.smartindent = true
+-- vim.opt.autoindent = true
+-- vim.opt.smartindent = true
+-- vim.opt.breakindent = true
 
 -- show sneaky characters
 vim.opt.list = true
@@ -36,9 +37,18 @@ vim.opt.listchars = {
 -- set bigger limit to allowed number of pages opened with "-p"
 vim.o.tabpagemax = 500
 
+-- Natural splits.
+-- Right and below, instead of left and above.
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
 --
 -- keybinds
 --
+
+-- Set space as the <leader> key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- disable accidental q key-press
 vim.keymap.set({'n', 'v'}, 'q:', '<Nop>') -- supposed to open cmdline window
