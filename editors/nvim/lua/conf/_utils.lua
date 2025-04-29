35a52770cd9c66cade1a18f9c72722dfacd5d791
juliancoffee <lightdarkdaughter@gmail.com>
2025-04-29 17:14:17 +0300

    (fixup/nvim) add utils file

---
+++ editors/nvim/lua/conf/_utils.lua
@@ -0,0 +1,9 @@
+-- INFO: module with various useful utilities
+
+local M = {}
+function M.is_termux()
+    -- check if `termux-setup-storage` command exists
+    return vim.fn.executable 'termux-setup-storage'
+end
+
+return M
