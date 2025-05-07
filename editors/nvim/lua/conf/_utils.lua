22cd30d198abd5e5dd59b07c03fe8ab0b26927fd
juliancoffee <lightdarkdaughter@gmail.com>
2025-05-07 23:42:35 +0300

    Add some comments to util functions

--- editors/nvim/lua/conf/_utils.lua
+++ editors/nvim/lua/conf/_utils.lua
@@ -1,11 +1,14 @@
 -- INFO: module with various useful utilities
 
 local M = {}
+
+--- Returns `true` if in `termux` environment
 function M.is_termux()
     -- check if `termux-setup-storage` command exists
     return vim.fn.executable('termux-setup-storage') == 1
 end
 
+--- Consume unused local to silence the diagnostic
 function M.fake_use(...) end
 
 return M
