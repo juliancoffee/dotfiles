279e128f44a004e84190cf1ad48a280ef3791d74
juliancoffee <lightdarkdaughter@gmail.com>
2026-05-02 00:46:52 +0300

    (nvim) dont bolt down mypy to uv

--- editors/nvim/lua/conf/linter.lua
+++ editors/nvim/lua/conf/linter.lua
@@ -11,17 +11,30 @@ return {
     event = { 'BufWritePre' },
     opts = function()
         local null_ls = require('null-ls')
-        local utils = require('null-ls.utils')
+        local config_utils = require('conf._utils')
+        local null_ls_utils = require('null-ls.utils')
         return {
             sources = {
                 null_ls.builtins.diagnostics.mypy.with {
-                    command = 'uv',
+                    command = function(params)
+                        if config_utils.is_uv_project(params.root) then
+                            return 'uv'
+                        else
+                            return 'mypy'
+                        end
+                    end,
                     args = function(params)
                         -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1208
-                        return {
-                            'run',
-                            '--dev',
-                            'mypy',
+                        local args = {}
+                        if config_utils.is_uv_project(params.root) then
+                            vim.list_extend(args, {
+                                'run',
+                                '--dev',
+                                'mypy',
+                            })
+                        end
+
+                        vim.list_extend(args, {
                             '--hide-error-context',
                             '--no-color-output',
                             '--show-column-numbers',
@@ -32,11 +45,12 @@ return {
                             params.bufname,
                             params.temp_path,
                             params.bufname,
-                        }
+                        })
+                        return args
                     end,
                     -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1208#issuecomment-1345356975
                     runtime_condition = function(params)
-                        return utils.path.exists(params.bufname)
+                        return null_ls_utils.path.exists(params.bufname)
                     end,
                 },
             },
